c5265bfb142fe6351ddd36b397c146b71f4cd2bc
lulucici <yolandachen7@zju.edu.cn>
2015-06-02 23:48:16 +0800

    c

--- 80021.c
+++ 80021.c
@@ -1,15 +1,3 @@
-// 程序填空，不要改变与输入输出有关的语句。
-// 输入5个字符串，输出其中最大的字符串。
-// 输入输出示例：括号内为说明
-// 输入：
-// peach 
-// pear
-// melon
-// orange
-// berry
-// 输出：
-// Max is: pear
-
 #include <stdio.h>
 #include <string.h>
 int main(void)
@@ -25,5 +13,5 @@ int main(void)
     	strcpy(max, strcmp(max, str)>0?max:str);
     	i++;
     }
-    printf("Max is：%s\n", max);
+    printf("Max is: %s\n", max);
 }   
