01f424996e4ff87722c4b2ebbc20976f4a10614c
lulucici <lulucici77@gmail.com>
2015-06-15 00:40:57 +0800

    c

---
+++ p10016.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+int main(void)
+{
+  int ri,repeat;
+  int i,n;
+  void dectobin(int n);
+
+  scanf("%d",&repeat);
+  for(ri=1;ri<=repeat;ri++){
+    scanf("%d",&n);
+    dectobin(n);
+    printf("\n");
+  }
+}
+
+void dectobin(int n)
+{
+	int result;
+
+	do{
+		i = 0;
+		/*......*/
+	}while( n != 0);
+
+}
